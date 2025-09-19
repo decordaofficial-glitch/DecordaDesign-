7a566fff87da4384c1c0582a09c25308a3a81ad1
Syeda <emanakhtar111@gmail.com>
2025-09-19 11:33:50 +0000

    Cant you insert google map on right side of contact us page "not image"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -415,7 +415,16 @@ export default function Home() {
                         </div>
                     </div>
                     <div className="relative h-[400px] rounded-lg overflow-hidden shadow-xl">
-                        <Image src="https://picsum.photos/seed/map/800/600" alt="Map" fill className="object-cover" data-ai-hint="store map" />
+                        <iframe
+                          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3619.434293937953!2d67.1147023150039!3d24.88313298404454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb3395a60a74b7f%3A0x94b411d3326fc49e!2sGulistan-e-Johar%2C%20Karachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2sus!4v1684484089941!5m2!1sen!2sus"
+                          width="100%"
+                          height="100%"
+                          style={{ border: 0 }}
+                          allowFullScreen={true}
+                          loading="lazy"
+                          referrerPolicy="no-referrer-when-downgrade"
+                          title="Google Map of Gulistan e Johar Branch"
+                        ></iframe>
                     </div>
                 </div>
             </div>
