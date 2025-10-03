f6965298aeefbd21e8fd3f1106ff2640cc939945
decordaofficial <decordaofficial@gmail.com>
2025-10-03 08:56:11 +0000

    Update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,24 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSbsZgpZ5e2-2oY2e7oW6xG1BqYqL3e9kC-c",
-  authDomain: "dev-project-362013.firebaseapp.com",
-  projectId: "dev-project-362013",
-  storageBucket: "dev-project-362013.appspot.com",
-  messagingSenderId: "447152922129",
-  appId: "1:447152922129:web:a62e32a67a05c117189a6a",
-  measurementId: "G-4999NC2N6C"
+  apiKey: "AIzaSyCUsw9ki-4CqmQIOBRvc91FkQhy6hAuX0A",
+  authDomain: "decordadesign.firebaseapp.com",
+  projectId: "decordadesign",
+  storageBucket: "decordadesign.firebasestorage.app",
+  messagingSenderId: "47395548581",
+  appId: "1:47395548581:web:b9e87aa1ccb7fcdb924c4c",
+  measurementId: "G-SDW3QR95XB"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// analytics is created here so it can be optionally used in other parts of the app
-// if it is not used, it will be tree-shaken away
-if (typeof window !== 'undefined') {
-  try {
-    const analytics = getAnalytics(app);
-  } catch (error) {
-    console.error('Failed to initialize Firebase Analytics', error);
-  }
-}
+const analytics = getAnalytics(app);
