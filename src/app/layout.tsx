a611e0770a01b9b5b1b7041013f9c332b4e24ba5
decordaofficial <decordaofficial@gmail.com>
2025-10-03 11:53:52 +0000

    Update fabicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Toaster } from "@/components/ui/toaster";
 export const metadata: Metadata = {
   title: 'Decorba',
   description: 'Event Backdrops and Party Supplies',
+  icons: {
+    icon: 'https://res.cloudinary.com/dh7m6g7f8/image/upload/v1759491772/Fabicon_32x32-03_murthm.png',
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +21,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="https://res.cloudinary.com/dh7m6g7f8/image/upload/v1759491772/Fabicon_32x32-03_murthm.png" />
       </head>
       <body className="font-sans antialiased bg-[#F8F8F8]">
         {children}
